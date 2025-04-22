d18a013ea9564d54619dc550d0c680c4b3b4b344
ramaravictor <ramaravictor07@gmail.com>
2025-04-22 15:55:11 +0700

    first commit

---
+++ assets/function.js
@@ -0,0 +1,11 @@
+video.addEventListener('click', () => {
+  alert('Ini Video Red Velvet');
+});
+
+audio.addEventListener('click', () => {
+  alert('Ini Audio Red Velvet');
+});
+
+gambar.addEventListener('click', function gambar(){
+  alert('Ini Gambar UMY');
+});
